cb2105100273ff244e2b38ea2d3d54ed5c8aa441
Jonasorz <chenzhen1208@gmail.com>
2026-07-27 16:09:14 +0800

    fix: hide overlapping development indicator

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  devIndicators: false,
   output: "standalone",
   poweredByHeader: false,
 };
 
 export default nextConfig;
-
